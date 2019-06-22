9d77e9c95ad438dfad5faca250297dea6f63f998
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-06-22 23:10:51 +0200

    Add support for rofi dmenu format
    The dmenu modi can be customized to provide different output formats.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,8 @@ import           Control.Monad
 import           Data.List (sort)
 import qualified Data.Text as T
 import           Prelude hiding (FilePath)
-import           Rofi
+import           Rofi hiding (s, d)
+import qualified Rofi as R
 import           System.Wordexp
 import           Turtle hiding (sort)
 
@@ -44,6 +45,7 @@ expand_path path = do
   expanded <- wordexp nosubst (encodeString path)
   return $ either (const []) (map fromString) expanded
 
+-- | Format project names suited to rofi selection list
 rofi_format_project_name :: FilePath -> IO Text
 rofi_format_project_name path = do
   home' <- home
@@ -57,18 +59,26 @@ rofi_format_project_name path = do
       Just rest -> "~" </> rest
   return $ format (s % " " % fp) name_padded dir
 
+-- | Build a help message of alternate commands with short description
 rofi_build_message :: [(Text, Text)] -> Text
 rofi_build_message = T.intercalate ", " . map format_command . zip [1 :: Int ..]
   where format_command (idx, (_, desc)) = format ("<b>Alt+"%d%"</b>: "%s) idx desc
 
-main :: IO ()
-main = do
-  projects <- find_projects ["/home/mmyrseth/src"]
+-- | Launch rofi with a list of projects as candidates
+rofi_projects :: [FilePath] -> IO ()
+rofi_projects source_dirs = do
+  projects <- find_projects source_dirs
   let candidates = traverse rofi_format_project_name (sort projects)
-      opts = rofi_prompt "Select project" <> rofi_msg (
-        rofi_build_message [("konsole", "Terminal")
-                           ,("emacs", "Editor")
-                           ,("dolphin", "Files")
-                           ])
+      opts =
+        rofi_prompt "Select project" <>
+        rofi_format R.q <>
+        rofi_msg (rofi_build_message
+                  [("konsole", "Terminal")
+                  ,("emacs", "Editor")
+                  ,("dolphin", "Files")
+                  ])
   result <- candidates >>= rofi opts
   print result
+
+main :: IO ()
+main = rofi_projects ["~/src", "~/projects"]
--- src/Rofi.hs
+++ src/Rofi.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Rofi
   ( rofi
+  , rofi_format
   , rofi_msg
   , rofi_prompt
+  , s, i, d, q, f, f'
   ) where
 
 import           Control.Arrow (second)
@@ -9,21 +13,59 @@ import           Data.List.NonEmpty (toList)
 import           Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import           Prelude hiding (FilePath)
-import           Turtle
+import           Turtle hiding (arg, s, d, f)
 
--- | Internal record type for cli options
+-- | Data type for command line options to rofi
 data RofiOpts = RofiOpts
-  { _msg :: Maybe Text
+  { _format :: Maybe RofiFormat
+  , _msg :: Maybe Text
   , _prompt :: Maybe Text
   }
 
+-- | Data type for supported -dmenu output formats
+data RofiFormat = FmtString
+                | FmtZeroIndex
+                | FmtOneIndex
+                | FmtQuote
+                | FmtFilter
+                | FmtFilterQuote
+
+-- | Output the selected string
+s :: RofiFormat
+s = FmtString
+
+-- | Output index (0 - (N-1))
+i :: RofiFormat
+i = FmtZeroIndex
+
+-- | Output index (1 - N)
+d :: RofiFormat
+d = FmtOneIndex
+
+-- | Quoted string
+q :: RofiFormat
+q = FmtQuote
+
+-- | Filter string (user input)
+f :: RofiFormat
+f = FmtFilter
+
+-- | Quoted filter string (user input)
+f' :: RofiFormat
+f' = FmtFilterQuote
+
 instance Semigroup RofiOpts where
-  left <> right = RofiOpts { _msg = _msg right <|> _msg left
+  left <> right = RofiOpts { _format = _format right <|> _format left
+                           , _msg = _msg right <|> _msg left
                            , _prompt = _prompt right <|> _prompt left
                            }
 
 instance Monoid RofiOpts where
-  mempty = RofiOpts Nothing Nothing
+  mempty = RofiOpts Nothing Nothing Nothing
+
+-- | Set the -format output of dmenu command line option
+rofi_format :: RofiFormat -> RofiOpts
+rofi_format fmt = mempty { _format = Just fmt }
 
 -- | Set -mesg command line option
 rofi_msg :: Text -> RofiOpts
@@ -38,8 +80,17 @@ rofi :: RofiOpts -> [Text] -> IO (ExitCode, Text)
 rofi opts candidates = do
   let input' = concatMap (toList . textToLines) candidates
       args = "-dmenu" : concat (
-        catMaybes [ _msg opts >>= pure . (["-mesg"] <>) . pure
-                  , _prompt opts >>= pure . (["-p"] <>) . pure
+        catMaybes [ _format opts >>= arg_fmt
+                  , _msg opts    >>= arg "-mesg"
+                  , _prompt opts >>= arg "-p"
                   ])
   print args
   second T.strip <$> procStrict "rofi" args (select input')
+  where arg key = pure . ([key] <>) . pure
+        arg_fmt = pure . (["-format"] <>) . pure . \case
+          FmtString -> "s"
+          FmtZeroIndex -> "i"
+          FmtOneIndex -> "d"
+          FmtQuote -> "q"
+          FmtFilter -> "f"
+          FmtFilterQuote -> "F"
