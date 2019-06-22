7e7455b5da41a836a267472c8252fcdcaefbd5fa
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-06-22 21:52:59 +0200

    Add "text"

--- derivation.nix
+++ derivation.nix
@@ -1,11 +1,23 @@
-{ mkDerivation, base, directory, foldl, stdenv, turtle, wordexp, buildTools ? [] }:
+{
+  mkDerivation,
+  stdenv,
+  base, directory, foldl, text, turtle, wordexp,
+  buildTools ? []
+}:
 mkDerivation {
   pname = "envix";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base directory foldl turtle wordexp ];
+  executableHaskellDepends = [
+    base
+    directory
+    foldl
+    text
+    turtle
+    wordexp
+  ];
   license = stdenv.lib.licenses.mit;
   inherit buildTools;
 }
