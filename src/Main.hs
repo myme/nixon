a2e109b77807f80fa6b7ebb32c5dfee088fb1485
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-06-22 21:53:10 +0200

    Get "rofi" invocation working

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
+import           Control.Arrow (second)
 import qualified Control.Foldl as Fold
 import           Control.Monad
 import           Data.List (sort)
+import           Data.List.NonEmpty (toList)
+import qualified Data.Text as T
 import           Prelude hiding (FilePath)
 import           System.Wordexp
 import           Turtle hiding (sort)
@@ -13,10 +16,12 @@ nix_files = ["shell.nix"
             ]
 
 project_markers :: [(FilePath -> IO Bool, FilePath)]
-project_markers = [(testdir, ".git")
+project_markers = nix_files' ++
+                  [(testdir, ".git")
                   ,(testdir, ".hg")
                   ,(testfile, ".project")
                   ]
+  where nix_files' = map ((,) testfile) nix_files
 
 is_project :: FilePath -> IO Bool
 is_project path = do
@@ -40,7 +45,27 @@ expand_path path = do
   expanded <- wordexp nosubst (encodeString path)
   return $ either (const []) (map fromString) expanded
 
+rofi :: [Text] -> IO (ExitCode, Text)
+rofi candidates = do
+  let input' = concatMap (toList . textToLines) candidates
+  second T.strip <$> procStrict "rofi" ["-dmenu"] (select input')
+
+rofi_format_project_name :: FilePath -> IO Text
+rofi_format_project_name path = do
+  home' <- home
+  let
+    name = format fp (basename path)
+    pad_width = 30
+    name_padded = name <> T.replicate (pad_width - T.length name) " "
+    parent' = parent path
+    dir = case stripPrefix (home' </> "") parent' of
+      Nothing -> parent'
+      Just rest -> "~" </> rest
+  return $ format (s % " " % fp) name_padded dir
+
 main :: IO ()
 main = do
-  projects <- find_projects (fromText <$> ["/home/mmyrseth/src"])
-  print (sort projects)
+  projects <- find_projects ["/home/mmyrseth/src"]
+  let candidates = sort projects
+  result <- traverse rofi_format_project_name candidates >>= rofi
+  print result
