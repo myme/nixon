0ca5773cc90a3c95e48b5c3b57ee305c5a83f76d
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-10-10 23:15:27 +0200

    Factor out Options merging into function

--- src/Envix/Config.hs
+++ src/Envix/Config.hs
@@ -55,6 +55,18 @@ read_config path = T.words <$> (readTextFile path `catch` as_empty)
   where as_empty :: IOError -> IO Text
         as_empty _ = pure ""
 
+merge_opts :: Options -> Options -> Options
+merge_opts secondary primary = Options
+  { project = project primary <|> project secondary
+  , backend = backend primary <|> backend secondary
+  , source_dirs = source_dirs secondary ++ source_dirs primary
+  , command = command primary <|> command secondary
+  , no_nix = no_nix primary || no_nix secondary
+  , config = config primary
+  , list = list primary
+  , select = select primary
+  }
+
 -- | Read configuration from config file and command line arguments
 parse_args :: IO Options
 parse_args = do
@@ -67,15 +79,7 @@ parse_args = do
     then pure cli_opts
     else do
       file_opts <- withArgs file_args arg_parser
-      pure $ Options { project = project cli_opts <|> project file_opts
-                     , backend = backend cli_opts <|> backend file_opts
-                     , source_dirs = source_dirs file_opts ++ source_dirs cli_opts
-                     , command = command cli_opts <|> command file_opts
-                     , no_nix = no_nix cli_opts || no_nix file_opts
-                     , config = config cli_opts
-                     , list = list cli_opts
-                     , select = select cli_opts
-                     }
+      pure $ merge_opts file_opts cli_opts
   if not . null $ source_dirs opts
     then pure opts
     else pure $ opts { source_dirs = ["~/src", "~/projects"] }
