f561f4385e43385cb3ed21781e0d735e1d46fc10
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-10-20 15:26:16 +0200

    Filter out code from main into functions
    Split main into a list and projectAction.

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,38 @@ import           Data.Maybe (fromMaybe)
 import qualified Data.Text.IO as T
 import qualified Envix.Config as Opts
 import           Envix.Fzf
+import           Envix.Process
 import           Envix.Projects
 import           Envix.Rofi hiding (d, s)
 import           Prelude hiding (FilePath)
 import qualified System.IO as IO
 import           Turtle hiding (decimal, find, sort, sortBy)
+
+-- | List projects, filtering if a filter is specified.
+list :: Maybe FilePath -> [Project] -> IO ()
+list project projects = do
+  paths <- fmap (format fp . project_path) <$> traverse implode_home projects
+  let fzf_opts = fzf_filter $ maybe "" (format fp) project
+  fzf fzf_opts paths >>= \case
+    FzfDefault matching -> T.putStr matching
+    _ -> T.hPutStrLn IO.stderr "No projects."
+
+-- | Find/filter out a project and perform an action
+projectAction :: Commands -> [Project] -> Opts.Options -> IO ()
+projectAction commands projects opts = do
+  def_backend <- bool Opts.Rofi Opts.Fzf <$> IO.hIsTerminalDevice IO.stdin
+  let backend = fromMaybe def_backend (Opts.backend opts)
+      (find, exec) = case backend of
+        Opts.Fzf -> (fzf_projects, fzf_exec)
+        Opts.Rofi -> (rofi_projects, rofi_exec)
+  action <- find (format fp <$> Opts.project opts) commands projects
+  case action of
+    Nothing -> do
+      T.hPutStrLn IO.stderr "No project selected."
+      exit (ExitFailure 1)
+    Just (project, command) -> if Opts.select opts
+      then printf (fp % "\n") (project_path project)
+      else exec (Opts.command opts <|> command) (Opts.no_nix opts) project
 
 -- TODO: Integrate with `direnv`
 -- TODO: Launch terminal with nix-shell output if taking a long time.
@@ -30,24 +57,5 @@ main = do
   projects <- sort_projects <$> find_projects source_dirs
 
   if Opts.list opts
-    then do
-      paths <- fmap (format fp . project_path) <$> traverse implode_home projects
-      let fzf_opts = fzf_filter $ maybe "" (format fp) (Opts.project opts)
-      fzf fzf_opts paths >>= \case
-        FzfDefault matching -> T.putStr matching
-        _ -> T.hPutStrLn IO.stderr "No projects."
-
-    else do
-      def_backend <- bool Opts.Rofi Opts.Fzf <$> IO.hIsTerminalDevice IO.stdin
-      let backend = fromMaybe def_backend (Opts.backend opts)
-          (find, exec) = case backend of
-            Opts.Fzf -> (fzf_projects, fzf_exec)
-            Opts.Rofi -> (rofi_projects, rofi_exec)
-      action <- find (format fp <$> Opts.project opts) commands projects
-      case action of
-        Nothing -> do
-          T.hPutStrLn IO.stderr "No project selected."
-          exit (ExitFailure 1)
-        Just (project, command) -> if Opts.select opts
-          then printf (fp % "\n") (project_path project)
-          else exec (Opts.command opts <|> command) (Opts.no_nix opts) project
+    then list (Opts.project opts) projects
+    else projectAction commands projects opts
