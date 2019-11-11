be221067f603e33d287d1666a0aacb42f9727da2
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-11-11 23:01:59 +0100

    todo: Add note

--- src/Envix/Config.hs
+++ src/Envix/Config.hs
@@ -11,6 +11,7 @@ import           Prelude hiding (FilePath)
 import           System.Environment (withArgs)
 import           Turtle hiding (select)
 
+-- TODO: Use Dhall
 -- TODO: Add CLI opt for outputting bash/zsh completion script.
 -- TODO: Add support for independent directory/tree of nix files.
 --       The idea is that for some projects you don't want to "pollute" the
