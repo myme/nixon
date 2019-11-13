a05835453864c4c0bd73639a735b5e4b0d1f6504
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-11-13 01:23:21 +0100

    tests: Update for new resolve_commands

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Envix.Projects
 import Envix.Projects.Types
+import Envix.Select
 import Test.Hspec
 
 main :: IO ()
@@ -8,9 +10,12 @@ main = hspec $ do
   describe "Projects" $
     describe "resolve_commands" $ do
       let project = from_path "/foo/bar/baz"
+          empty = const (pure EmptySelection)
 
       it "joins parts with spaces" $ do
-        resolve_command project "this is a command" `shouldBe` "this is a command"
+        res <- runSelect empty $ resolve_command project "this is a command"
+        res `shouldBe` "this is a command"
 
       it "joins parts with spaces" $ do
-        resolve_command project ("this is:" <> path) `shouldBe` "this is: /foo/bar/baz"
+        res <- runSelect empty $ resolve_command project ("this is:" <> path)
+        res `shouldBe` "this is: /foo/bar/baz"
