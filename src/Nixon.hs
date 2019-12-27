366fc9753757e17e3b4f9a6a104adfbaa7dfa426
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-12-27 15:46:51 +0100

    Switch from ExceptT to IO errors

--- src/Nixon.hs
+++ src/Nixon.hs
@@ -3,8 +3,7 @@ module Nixon
   , nixon_with_config
   ) where
 
-import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Except
+import           Control.Exception
 import           Control.Monad.Trans.Maybe
 import           Data.Maybe (fromMaybe)
 import qualified Data.Text.IO as T
@@ -23,7 +22,7 @@ import           Nixon.Rofi
 import           Nixon.Select hiding (select)
 import           Nixon.Types
 import           Prelude hiding (FilePath, log)
-import           Turtle hiding (decimal, env, err, find, shell)
+import           Turtle hiding (decimal, env, err, find, shell, x)
 
 -- | List projects, filtering if a filter is specified.
 list :: [Project] -> Maybe Text -> IO ()
@@ -46,13 +45,14 @@ build_action opts = do
     ExitFailure _ -> putStrLn "Compilation failed!"
     ExitSuccess -> putStrLn "Compilation successful!"
 
-on_empty :: (Monad m) => Text -> m (Maybe a) -> ExceptT Text m a
-on_empty err = maybeToExceptT err . MaybeT
+newtype NixonError = EmptyError Text deriving Show
 
-handleExcept :: (MonadIO m) => ExceptT Text m () -> m ()
-handleExcept action = runExceptT action >>= \case
-  Left err -> printErr err >> exit (ExitFailure 1)
-  Right _ -> pure ()
+instance Exception NixonError
+
+fail_empty :: (MonadIO m) => Text -> m (Maybe a) -> m a
+fail_empty err action = action >>= \case
+  Nothing -> liftIO (throwIO $ EmptyError err)
+  Just x -> pure x
 
 -- | Maybe wrap a command in direnv/nix.
 maybe_wrap_cmd :: Project -> Command -> Nixon Command
@@ -65,11 +65,11 @@ run_cmd :: (Maybe Text -> Project -> IO (Maybe Command))
              -> Project
              -> ProjectOpts
              -> Selector
-             -> ExceptT Text Nixon ()
+             -> Nixon ()
 run_cmd find_command project opts selector = do
-  cmd <- on_empty "No command selected." $ liftIO $ find_command (Options.command opts) project
-  cmd' <- lift $ maybe_wrap_cmd project cmd
-  lift $ log_info (format ("Running command '"%w%"'") cmd')
+  cmd <- liftIO $ fail_empty "No command selected." $ find_command (Options.command opts) project
+  cmd' <- maybe_wrap_cmd project cmd
+  log_info (format ("Running command '"%w%"'") cmd')
   liftIO $ runSelect selector $ project_exec cmd' project
 
 -- | Find/filter out a project and perform an action.
@@ -90,11 +90,10 @@ project_action projects opts
             <|> MaybeT (find_project Nothing projects)
           find_project' query = find_project query projects
 
-      handleExcept $ do
-        project <- on_empty "No project selected." $ liftIO $ find_project' (Options.project opts)
-        if Options.select opts
-          then liftIO $ printf (fp % "\n") (project_path project)
-          else run_cmd find_command project opts selector
+      project <- liftIO $ fail_empty "No project selected." $ find_project' (Options.project opts)
+      if Options.select opts
+        then liftIO $ printf (fp % "\n") (project_path project)
+        else run_cmd find_command project opts selector
 
 -- | Run a command from current directory
 run_action :: ProjectOpts -> Nixon ()
@@ -104,11 +103,10 @@ run_action opts = do
       (find_command, selector) = case backend env of
         Fzf -> (fzf_project_command, fzf_with_edit mempty)
         Rofi -> (rofi_project_command, rofi mempty)
-  handleExcept $ do
-    project <- liftIO $ do
-      current <- from_path <$> pwd
-      fromMaybe current <$> (find_in_project ptypes =<< pwd)
-    run_cmd find_command project opts selector
+  project <- liftIO $ do
+    current <- from_path <$> pwd
+    fromMaybe current <$> (find_in_project ptypes =<< pwd)
+  run_cmd find_command project opts selector
 
 -- TODO: Launch terminal with nix-shell output if taking a long time.
 -- TODO: Allow changing default command
@@ -119,7 +117,7 @@ run_action opts = do
 nixon_with_config :: Config -> IO ()
 nixon_with_config user_config = do
   opts <- either print_error pure =<< Options.parse_args
-  runNixon opts user_config $ case Options.sub_command opts of
+  err <- try $ runNixon opts user_config $ case Options.sub_command opts of
     BuildCommand build_opts -> do
       log_info "Running <build> command"
       liftIO (build_action build_opts)
@@ -133,6 +131,9 @@ nixon_with_config user_config = do
     RunCommand run_opts -> do
       log_info "Running <run> command"
       run_action run_opts
+  case err of
+    Left (EmptyError msg) -> print_error msg
+    Right _ -> pure ()
   where print_error err = printErr err >> exit (ExitFailure 1)
 
 default_config :: Config
