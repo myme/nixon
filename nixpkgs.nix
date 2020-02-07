1ad305a851295b0bd8a209ceed02ccf9b7cf44a4
Martin Øinæs Myrseth <myrseth@gmail.com>
2020-02-07 20:57:03 +0100

    nixpkgs: Add refs to fetchGit
    Seems like fetchGit doesn't necessarily fetch entire repos or filter by
    remote refs or something like it. That means it might not find revisions
    specified without a ref to go with it.
    
    https://github.com/NixOS/nix/issues/2431

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,13 @@
 let
   nixpkgs = import (builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs-channels";
+    ref = "nixos-19.09";
     rev = "2de9367299f325c2b2021a44c2f63c810f8ad023";
   });
 
   koi = import (builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs-channels";
+    ref = "nixos-19.03";
     rev = "c8db7a8a16ee9d54103cade6e766509e1d1c8d7b";
   });
 
