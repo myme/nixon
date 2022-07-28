5560588ec165812213850163c60110a2b3f618ce
Martin Myrseth <mm@myme.no>
2022-07-28 11:33:38 +0200

    flake: Update nixpkgs to latest 22.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nixon nix flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
   outputs = { self, nixpkgs }:
     let system = "x86_64-linux";
