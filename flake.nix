d1549f3e0397665dac89ae40d08b047f4cd5f799
Martin Myrseth <mm@myme.no>
2023-01-03 08:32:19 +0100

    nixpkgs: Upgrade to 22.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nixon nix flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }:
     let system = "x86_64-linux";
