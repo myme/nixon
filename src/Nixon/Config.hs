382e500bf390a8d7d19a8f834f9ea4d3754c8cf5
Martin Myrseth <mm@myme.no>
2023-04-12 23:21:54 +0200

    config: Allow both "nixon.md" and ".nixon.md"
    No reason to require hidden goodness.

--- src/Nixon/Config.hs
+++ src/Nixon/Config.hs
@@ -8,6 +8,7 @@ import Control.Exception (throwIO)
 import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
 import Data.Bifunctor (Bifunctor (first))
 import Data.Char (isSpace)
+import Data.Monoid (First (First, getFirst))
 import qualified Data.Text as T
 import Nixon.Config.Markdown (parseMarkdown)
 import Nixon.Config.Types (Config, ConfigError (..))
@@ -18,12 +19,17 @@ import Turtle (readTextFile)
 
 findLocalConfig :: MonadIO m => FilePath -> m (Maybe Config)
 findLocalConfig path = runMaybeT $ do
-  local_config <- MaybeT $ find_dominating_file path ".nixon.md"
+  local_config <- MaybeT $ firstOf (find_dominating_file path) ["nixon.md", ".nixon.md"]
   res <- readConfig local_config
   MaybeT $ either onError (pure . Just) res
   where
     onError (ParseError err) = liftIO $ throwIO (ParseError err)
     onError _ = pure Nothing
+
+-- | Get the first non-Nothing value from an applicative operation applied to a
+-- list of inputs.
+firstOf :: Applicative f => (a -> f (Maybe b)) -> [a] -> f (Maybe b)
+firstOf f xs = getFirst . mconcat . map First <$> traverse f xs
 
 readConfig :: MonadIO m => FilePath -> m (Either ConfigError Config)
 readConfig path = do
