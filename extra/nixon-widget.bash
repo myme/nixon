0d2c35f9423de03f2b02b5b323aec9f261c45002
Martin Myrseth <mm@myme.no>
2024-03-17 01:05:10 +0100

    bash: Add nixon-insert-project
    Add a binding in `bash` to insert the path to a fuzzy found nixon
    project into the prompt.

--- extra/nixon-widget.bash
+++ extra/nixon-widget.bash
@@ -5,3 +5,12 @@ nixon-widget() {
 }
 
 bind -x '"\ei": "nixon-widget"'
+
+nixon-insert-project () 
+{
+    local project="$(nixon -b fzf -T project -s)"
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$project${READLINE_LINE:$READLINE_POINT}";
+    READLINE_POINT=$(( READLINE_POINT + ${#project} ))
+}
+
+bind -x '"\ep": nixon-insert-project'
