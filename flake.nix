33232293880e71f993629150f6c832d197b19cbe
Martin Myrseth <mm@myme.no>
2024-05-08 21:08:09 +0200

    Update nixpkgs to 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nixon nix flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs }:
     let
