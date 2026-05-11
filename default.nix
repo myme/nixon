f53c0d9435412fab1d2b95cae720479db3074fe2
Martin Myrseth <mm@myme.no>
2026-05-11 06:07:24 +0200

    flake: Support default systems

--- default.nix
+++ default.nix
@@ -1,29 +1,35 @@
-{ pkgs ? import ./nixpkgs.nix {}
-, haskellPackages ? pkgs.haskellPackages,
+{
+  pkgs ? import ./nixpkgs.nix { },
+  haskellPackages ? pkgs.haskellPackages,
 }:
 
 let
   gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   src = gitignore ./.;
 
-in (haskellPackages.callCabal2nix "nixon" ./. {}).overrideAttrs (attrs: attrs // {
- executableSystemDepends = with pkgs; [
-   fzf
-   rofi
- ];
-  postInstall = ''
-    # Install widgets into share/nixon
-    mkdir -p $out/share/nixon
-    install ${src}/extra/nixon-widget.bash $out/share/nixon
-    install ${src}/extra/nixon-widget.zsh $out/share/nixon
+in
+(haskellPackages.callCabal2nix "nixon" ./. { }).overrideAttrs (
+  attrs:
+  attrs
+  // {
+    executableSystemDepends = with pkgs; [
+      fzf
+      rofi
+    ];
+    postInstall = ''
+      # Install widgets into share/nixon
+      mkdir -p $out/share/nixon
+      install ${src}/extra/nixon-widget.bash $out/share/nixon
+      install ${src}/extra/nixon-widget.zsh $out/share/nixon
 
-    # Bash completions
-    mkdir -p $out/share/bash-completion/completions
-    $out/bin/nixon --bash-completion-script $out/bin/nixon > $out/share/bash-completion/completions/nixon.bash
+      # Bash completions
+      mkdir -p $out/share/bash-completion/completions
+      $out/bin/nixon --bash-completion-script $out/bin/nixon > $out/share/bash-completion/completions/nixon.bash
 
-    # Install zsh widget + completions into share/zsh/site-functions
-    mkdir -p $out/share/zsh/site-functions
-    install ${src}/extra/nixon-widget.zsh $out/share/zsh/site-functions/_nixon_widget
-    $out/bin/nixon --zsh-completion-script $out/bin/nixon > $out/share/zsh/site-functions/_nixon_completion
-  '';
-})
+      # Install zsh widget + completions into share/zsh/site-functions
+      mkdir -p $out/share/zsh/site-functions
+      install ${src}/extra/nixon-widget.zsh $out/share/zsh/site-functions/_nixon_widget
+      $out/bin/nixon --zsh-completion-script $out/bin/nixon > $out/share/zsh/site-functions/_nixon_completion
+    '';
+  }
+)
--- flake.nix
+++ flake.nix
@@ -1,32 +1,44 @@
 {
   description = "Nixon nix flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-    in {
-      overlay = (final: prev: {
-        nixon = pkgs.haskell.lib.compose.justStaticExecutables
-          final.haskellPackages.nixon;
-        haskellPackages = prev.haskellPackages // {
-          nixon = import ./default.nix {
-            pkgs = final;
-            inherit (final) haskellPackages;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    {
+      overlay = (
+        final: prev: {
+          nixon = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.nixon;
+          haskellPackages = prev.haskellPackages // {
+            nixon = import ./default.nix {
+              pkgs = final;
+              inherit (final) haskellPackages;
+            };
           };
+        }
+      );
+    }
+    // (utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
         };
-      });
-
-      defaultPackage.${system} = pkgs.nixon;
-
-      devShell.${system} = import ./shell.nix {
-        inherit pkgs;
-        inherit (pkgs) haskellPackages;
-      };
-    };
+      in
+      {
+        defaultPackage = pkgs.nixon;
+        devShell = import ./shell.nix {
+          inherit pkgs;
+          inherit (pkgs) haskellPackages;
+        };
+      }
+    ));
 }
