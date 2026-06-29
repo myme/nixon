1005ea00f5b9096c1617ee6fc3ffec50bdca195e
Martin Myrseth <mm@myme.no>
2026-06-29 09:52:59 +0200

    deps: Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixon nix flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     utils.url = "github:numtide/flake-utils";
   };
 
